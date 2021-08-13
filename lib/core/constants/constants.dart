073ab068c4c52a0d5831dee6f2b58f16d67ab478
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-13 14:54:53 +0200

    Add some colors and sizes to constants  - #4

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+const kBgColor = Color(0xFFF6F5F2);
+const primaryColor = Color(0xFF40A944);
 const kPrimaryColor = Color(0xFF3E4067);
 const kTextColor = Color(0xFF3F4168);
 const kIconColor = Color(0xFF5E5E5E);
 
 const kDefaultPadding = 20.0;
 const kStatusImageRadius = 50.0;
+const kCartBarHeight = 100.0;
+const kHeaderHeight = 185.0;
 
 final kDefaultShadow = BoxShadow(
   offset: Offset(5, 5),
   blurRadius: 10,
   color: Color(0xFFE9E9E9).withOpacity(0.56),
 );
+
+const kPanelTransition = Duration(milliseconds: 500);
