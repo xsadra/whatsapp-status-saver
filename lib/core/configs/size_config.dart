bb2ab785b05c17c7656ff257fcd3c71e6ff525c9
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-13 14:56:52 +0200

    Remove unused codes  - #4

--- lib/core/configs/size_config.dart
+++ lib/core/configs/size_config.dart
@@ -15,21 +15,16 @@ class SizeConfig {
   }
 }
 
-// Get the proportionate height as per screen size
 double getProportionateScreenHeight(double inputHeight) {
   double screenHeight = SizeConfig.screenHeight;
-  // Our designer use iPhone 11 Pro, that's why we use 815.0
   return (inputHeight / 815.0) * screenHeight;
 }
 
-// Get the proportionate height as per screen size
 double getProportionateScreenWidth(double inputWidth) {
   double screenWidth = SizeConfig.screenWidth;
-  // 414 is the layout width that designer use or you can say iPhone 11 Pro width
   return (inputWidth / 414.0) * screenWidth;
 }
 
-// For add free space vertically
 class VerticalSpacing extends StatelessWidget {
   const VerticalSpacing({
     Key? key,
--- lib/status_saver/presentation/widgets/widgets.dart
+++ lib/status_saver/presentation/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'home_screen/circular_avatar.dart';
 export 'home_screen/home_header.dart';
-export 'home_screen/section_title.dart';
-export 'home_screen/status_image.dart';
-export 'home_screen/status_image_collection.dart';
+export 'home_screen/home_long_list.dart';
+export 'home_screen/home_short_list.dart';
