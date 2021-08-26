d9b9ea53969c64eec8eaf61584e362024ea7504b
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-26 20:50:11 +0200

    Create 'Account' and add 'AccountTypeExtension' to 'AccountType'

--- lib/status_saver/data/models/account_type.dart
+++ lib/status_saver/data/models/account_type.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/foundation.dart';
+
 enum AccountType {
   WhatsApp,
   WhatsAppBusiness,
+}
+
+extension AccountTypeExtension on AccountType {
+  String get name => describeEnum(this);
+
+  String get value {
+    switch (this) {
+      case AccountType.WhatsApp:
+        return 'WhatsApp';
+      case AccountType.WhatsAppBusiness:
+        return 'WhatsApp Business';
+      default:
+        return 'not found';
+    }
+  }
 }
