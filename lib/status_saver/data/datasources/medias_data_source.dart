d6e9f7653bcae72bf2ac30e374bc69c1b73897ce
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-26 20:51:31 +0200

    Implement 'MediasDataSource'

--- lib/status_saver/data/datasources/medias_data_source.dart
+++ lib/status_saver/data/datasources/medias_data_source.dart
@@ -1,27 +1,54 @@
-import 'package:whatsapp_status_saver/status_saver/data/models/account_type.dart';
+import 'dart:io';
+
+import '../models/account_type.dart';
+import '../models/medias.dart';
 
 abstract class MediasDataSource {
-  Map<AccountType, List<Uri>> getAccountMedias();
-  bool hasWhatsApp();
-  bool hasWhatsAppBusiness();
+  Map<AccountType, Medias> getAccountMedias();
+
+  bool? hasData(AccountType type);
 }
 
 class MediasDataSourceImpl implements MediasDataSource {
-  @override
-  Map<AccountType, List<Uri>> getAccountMedias() {
-    // TODO: implement getAccountMedias
-    throw UnimplementedError();
+  Map<AccountType, bool> _accountHasData = {};
+
+  List<Uri> _getStorages() {
+    //Todo: get available storages and return their Uris
+    return [
+      Uri.parse('storage/emulated/0'),
+      Uri.parse('storage/emulated/0'),
+      Uri.parse('storage/emulated/1'),
+    ];
   }
 
   @override
-  bool hasWhatsApp() {
-    // TODO: implement hasWhatsApp
-    throw UnimplementedError();
+  Map<AccountType, Medias> getAccountMedias() {
+    Map<AccountType, Medias> accountMedias = {};
+
+    List<Uri> storages = _getStorages()
+        .where((storage) => Directory.fromUri(storage).existsSync())
+        .toList(growable: false);
+
+    List<FileSystemEntity> fileSystemEntities = [
+      for (var storage in storages) ...Directory.fromUri(storage).listSync()
+    ];
+
+    for (AccountType type in AccountType.values) {
+      for (FileSystemEntity entity in fileSystemEntities) {
+        if (entity.path.endsWith(type.value)) {
+          Medias accountMedia = accountMedias[type] ?? Medias(uris: []);
+          accountMedia.uris.add(entity.uri);
+          accountMedias[type] = accountMedia;
+          _accountHasData[type] = true;
+        }
+      }
+    }
+
+    return accountMedias;
   }
 
   @override
-  bool hasWhatsAppBusiness() {
-    // TODO: implement hasWhatsAppBusiness
-    throw UnimplementedError();
+  bool? hasData(AccountType type) {
+    return _accountHasData[type];
   }
 }
