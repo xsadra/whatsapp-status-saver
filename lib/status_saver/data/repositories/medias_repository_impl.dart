eb3b754ec471b9b8ba119732e7e5613938f00e87
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-27 14:01:42 +0200

    Modify 'MediasRepositoryImpl' to fix separate uris #11

--- lib/status_saver/data/repositories/medias_repository_impl.dart
+++ lib/status_saver/data/repositories/medias_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 
 import '../../../core/error/failure.dart';
@@ -20,25 +22,44 @@ class MediasRepositoryImpl implements MediasRepository {
       Map<AccountType, Medias> accountMedias = dataSource.getAccountMedias();
 
       AccountMedias accountMediaSeparated = {};
-      accountMedias.forEach((key, value) {
-        Map<MediaType, Medias> mediaSeparated = {};
-        for (var uri in value.uris) {
-          if (uri.path.endsWith(MediaType.Image.value)) {
-            Medias media = mediaSeparated[MediaType.Image] ?? Medias(uris: []);
-            media.uris.add(uri);
-            mediaSeparated[MediaType.Image] = media;
-          } else if (uri.path.endsWith(MediaType.Video.value)) {
-            Medias media = mediaSeparated[MediaType.Video] ?? Medias(uris: []);
-            media.uris.add(uri);
-            mediaSeparated[MediaType.Video] = media;
-          }
-        }
-        accountMediaSeparated[key] = mediaSeparated;
-      });
+
+      for (var entry in accountMedias.entries) {
+        Map<MediaType, Medias> mediaSeparated =
+            _extractMediaSeparated(entry.key, entry.value);
+        accountMediaSeparated[entry.key] = mediaSeparated;
+      }
 
       return Future.value(right(accountMediaSeparated));
     } catch (e) {
       return Future.value(left(ReadWriteException()));
     }
+  }
+
+  Map<MediaType, Medias> _extractMediaSeparated(AccountType key, Medias value) {
+    Map<MediaType, Medias> mediaSeparated = {};
+    for (var rootUri in value.uris) {
+      Directory systemTempDir = Directory.fromUri(rootUri);
+
+      List<FileSystemEntity> listSync = systemTempDir
+          .listSync(recursive: true, followLinks: true)
+          .where((event) => event.path.contains('Media/.Statuses/'))
+          .toList();
+
+      for (FileSystemEntity entity in listSync) {
+        Uri uri = entity.uri;
+        if (uri.path.endsWith(MediaType.Image.value)) {
+          Medias media = mediaSeparated[MediaType.Image] ?? Medias(uris: []);
+          media.uris.add(uri);
+          mediaSeparated[MediaType.Image] = media;
+        } else if (uri.path.endsWith(MediaType.Video.value)) {
+          Medias media = mediaSeparated[MediaType.Video] ?? Medias(uris: []);
+          media.uris.add(uri);
+          mediaSeparated[MediaType.Video] = media;
+        } else {
+          print('not supported Uri in:' + key.value + ' -> ' + uri.toString());
+        }
+      }
+    }
+    return mediaSeparated;
   }
 }
