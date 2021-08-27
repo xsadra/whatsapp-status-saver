94b91acf55514ce5537f7a3bb70b0bc0f04d7cfc
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-27 11:48:23 +0200

    Add 'toMessage' Extension to 'Failure'

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -5,3 +5,18 @@ class ReadWriteException extends Failure {}
 class PermissionException extends Failure {}
 
 class NotFoundFailure extends Failure {}
+
+extension FailureExtension on Failure {
+  String get toMessage {
+    switch (this.runtimeType) {
+      case ReadWriteException:
+        return 'Can not Read/Write on Storage';
+      case PermissionException:
+        return 'No Permission';
+      case NotFoundFailure:
+        return 'Not Found';
+      default:
+        return 'Unexpected Error';
+    }
+  }
+}
