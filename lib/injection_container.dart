99f472a034172509f8c67c02d9e5e3525b81a3ad
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-27 11:52:25 +0200

    Inject MediasBloc dependency

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,13 +3,14 @@ import 'package:get_it/get_it.dart' show GetIt;
 import 'status_saver/data/datasources/medias_data_source.dart';
 import 'status_saver/data/repositories/medias_repository_impl.dart';
 import 'status_saver/domain/repositories/medias_repository.dart';
+import 'status_saver/presentation/bloc/medias/bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-// sl.registerFactory(
-//       () => MediaBloc(media: sl()),
-// );
+  sl.registerFactory(
+    () => MediasBloc(repository: sl()),
+  );
 
   sl.registerLazySingleton<MediasRepository>(
     () => MediasRepositoryImpl(
