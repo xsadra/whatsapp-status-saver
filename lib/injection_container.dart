de60ffa906909586a290444c42305a5a7a825bd0
Sadra <31988724+xsadra@users.noreply.github.com>
2021-08-30 18:55:36 +0200

    Inject 'SavedMedia' dependency in 'injection_container' #16

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -4,12 +4,16 @@ import 'status_saver/data/datasources/medias_data_source.dart';
 import 'status_saver/data/repositories/medias_repository_impl.dart';
 import 'status_saver/domain/repositories/medias_repository.dart';
 import 'status_saver/presentation/bloc/medias/bloc.dart';
+import 'status_saver/presentation/bloc/saved_medias/bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   sl.registerFactory(
     () => MediasBloc(repository: sl()),
+  );
+  sl.registerFactory(
+    () => SavedMediasBloc(repository: sl()),
   );
 
   sl.registerLazySingleton<MediasRepository>(
