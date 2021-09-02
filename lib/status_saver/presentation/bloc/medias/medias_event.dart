36edaa0768076570a8ac1eb140315d366392e624
Sadra <31988724+xsadra@users.noreply.github.com>
2021-09-02 23:46:47 +0200

    Refactor 'GetAccountMedias' event #16

--- lib/status_saver/presentation/bloc/medias/medias_event.dart
+++ lib/status_saver/presentation/bloc/medias/medias_event.dart
@@ -1,9 +1,15 @@
 import 'package:meta/meta.dart' show immutable;
 
+import '../../../data/models/models.dart' show AccountType;
+
 @immutable
 abstract class MediasEvent {}
 
-class GetAccountMedias extends MediasEvent {}
+class GetAccountMedias extends MediasEvent {
+  final AccountType setAccountToShow;
+
+  GetAccountMedias({required this.setAccountToShow});
+}
 
 class SaveMedia extends MediasEvent {
   final Uri uri;
