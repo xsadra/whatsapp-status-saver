6e5923579409608172854143e8dd67fb46c9de01
Sadra <31988724+xsadra@users.noreply.github.com>
2021-09-04 11:17:48 +0200

    Refactor 'DetailsScreen' to show video detail #20

--- lib/status_saver/presentation/pages/detail_screen.dart
+++ lib/status_saver/presentation/pages/detail_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart' show ShowMediaDetail;
 
 class DetailsScreen extends StatefulWidget {
   final Uri uri;
@@ -25,41 +25,49 @@ class _DetailsScreenState extends State<DetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEEEEEE),
-      body: Container(
-        color: Colors.transparent,
-        child: Center(
-          child: Column(
-            children: [
-              Spacer(
-                flex: 5,
+      body: buildBody(),
+      bottomNavigationBar: buildBottomNavigationBar(context),
+    );
+  }
+
+  Widget buildBody() {
+    return Container(
+      color: Colors.transparent,
+      child: Center(
+        child: Column(
+          children: [
+            Spacer(
+              flex: 5,
+            ),
+            Hero(
+              tag: widget.uri.path + tag,
+              child: ShowMediaDetail(
+                uri: widget.uri,
+                height: widget.height,
               ),
-              Hero(
-                tag: widget.uri.path + tag,
-                child: Image.file(
-                  File.fromUri(widget.uri),
-                  height: widget.height,
-                ),
-              ),
-              Spacer(
-                flex: 1,
-              ),
-            ],
-          ),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+          ],
         ),
       ),
-      bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 20),
-          child: ElevatedButton(
-            onPressed: () {
-              widget.onSave();
-              setState(() {
-                tag = 'tag';
-              });
-              Navigator.pop(context);
-            },
-            child: Text('Save'),
-          ),
+    );
+  }
+
+  Widget buildBottomNavigationBar(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 20),
+        child: ElevatedButton(
+          onPressed: () {
+            widget.onSave();
+            setState(() {
+              tag = 'tag';
+            });
+            Navigator.pop(context);
+          },
+          child: Text('Save'),
         ),
       ),
     );
