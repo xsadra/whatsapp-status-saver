840945d007c0aecbb4ccaab16d60d41649963068
Sadra <31988724+xsadra@users.noreply.github.com>
2021-09-11 23:31:55 +0200

    Add a logger (log manager) to the app #33

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:whatsapp_status_saver/core/logs/logger.dart';
 
-import 'injection_container.dart' as injection;
+import 'injection_container.dart' as injection.dart';
 import 'status_saver/data/models/models.dart';
 import 'status_saver/presentation/bloc/medias/bloc.dart';
 import 'status_saver/presentation/bloc/saved_medias/bloc.dart';
@@ -19,6 +21,8 @@ void main() async {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    logs.v('build MainApp');
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Status Saver',
