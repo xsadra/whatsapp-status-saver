289c96ae6f8f873474c109bd99daf220a6924693
Sadra <31988724+xsadra@users.noreply.github.com>
2021-09-13 23:55:03 +0200

    Setup Setting Screen to show on Home Screen #7

--- lib/status_saver/presentation/widgets/home_screen/home_header.dart
+++ lib/status_saver/presentation/widgets/home_screen/home_header.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whatsapp_status_saver/status_saver/presentation/pages/pages.dart';
 
 import '../../../../core/constants/constants.dart'
-    show kDefaultPadding, kHeaderHeight;
+    show kDefaultPadding, kHeaderHeight, kPanelTransition;
 import '../../../data/models/models.dart';
 import '../../bloc/medias/bloc.dart';
 
@@ -61,56 +62,92 @@ class HomeHeader extends StatelessWidget {
                   ],
                 ),
                 Spacer(),
-                BlocBuilder<MediasBloc, MediasState>(
-                  buildWhen: (previous, current) =>
-                      previous != current && current.state is Loaded,
-                  builder: (context, state) {
-                    if (state is Loaded) {
-                      var toShow = state.accountToShow;
-
-                      return Row(
-                        children: [
-                          for (AccountType account in state.accounts)
-                            Container(
-                              padding: const EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                // color: const Color(0xFF4C0050),
-                                border: account.isOf(toShow)
-                                    ? Border(
-                                        bottom: BorderSide(
-                                            width: 4, color: Colors.black26))
-                                    : null,
-                              ),
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 4.0),
-                                child: InkWell(
-                                  onTap: () => context
-                                      .read<MediasBloc>()
-                                      .add(GetAccountMedias(
-                                        setAccountToShow: account,
-                                      )),
-                                  child: Image.asset(
-                                    account.isOf(toShow)
-                                        ? account.imagePath
-                                        : account.saturatedImagePath,
-                                    width: account.isOf(toShow) ? 36 : 40,
-                                    filterQuality: FilterQuality.high,
-                                  ),
-                                ),
-                              ),
-                            ),
-                        ],
-                      );
-                    }
-                    return Container();
-                  },
-                ),
               ],
             ),
           ),
         ),
+        Positioned(
+          bottom: 5,
+          right: 14,
+          child: BlocBuilder<MediasBloc, MediasState>(
+            buildWhen: (previous, current) =>
+                previous != current && current.state is Loaded,
+            builder: (context, state) {
+              if (state is Loaded) {
+                var toShow = state.accountToShow;
+
+                return Row(
+                  children: [
+                    for (AccountType account in state.accounts)
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          // color: const Color(0xFF4C0050),
+                          border: account.isOf(toShow)
+                              ? Border(
+                                  bottom: BorderSide(
+                                      width: 4, color: Colors.black26))
+                              : null,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                          child: InkWell(
+                            onTap: () =>
+                                context.read<MediasBloc>().add(GetAccountMedias(
+                                      setAccountToShow: account,
+                                    )),
+                            child: Image.asset(
+                              account.isOf(toShow)
+                                  ? account.imagePath
+                                  : account.saturatedImagePath,
+                              width: account.isOf(toShow) ? 36 : 40,
+                              filterQuality: FilterQuality.high,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                );
+              }
+              return Container();
+            },
+          ),
+        ),
+        Positioned(
+          top: 16,
+          right: 10,
+          child: GestureDetector(
+            onTap: () {
+              navigateToSettingScreen(context);
+            },
+            child: Hero(
+              tag: 'setting',
+              child: Icon(
+                Icons.more_vert,
+                size: 40,
+                color: Color(0xBB000000),
+              ),
+            ),
+          ),
+        ),
       ],
+    );
+  }
+
+  Future<dynamic> navigateToSettingScreen(BuildContext buildContext) {
+    return Navigator.push(
+      buildContext,
+      PageRouteBuilder(
+        fullscreenDialog: true,
+        transitionDuration: kPanelTransition,
+        reverseTransitionDuration: kPanelTransition,
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FadeTransition(
+            opacity: animation,
+            child: SettingScreen(),
+          );
+        },
+      ),
     );
   }
 }
